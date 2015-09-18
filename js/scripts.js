09e70e2ea40bf668e2870e8ab1c7dde421dcf602
cdg77 <cdegregorio@gmail.com>
2015-09-18 14:28:28 -0700

    return a customer name

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function Order(customerName) {
+  this.customerName = customerName;
+}
+
 function Pizza(topping1) {
   this.topping1 = topping1;
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,10 @@
+describe('Order', function() {
+  it('returns an order with a customer', function() {
+    var testOrder = new Order('Luigi');
+    expect(testOrder.customerName).to.equal('Luigi');
+  });
+});
+
 describe('Pizza', function() {
   it('return a pizza with one topping', function() {
     var testPizza = new Pizza('carnitas');
